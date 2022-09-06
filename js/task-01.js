7d24fef459f3d254ada62055dae647529a48ccca
Serhii Bakun <gizzmo599@gmail.com>
2022-09-06 21:24:25 +0300

    task-01

---
+++ js/task-01.js
@@ -0,0 +1,17 @@
+// 1. Порахує і виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
+
+const categoriesEl = document.querySelector('#categories');
+console.log(`Number of categories: ${categoriesEl.children.length}`);
+
+// 2. Для кожного элемента li.item у спику ul#categories, знайде і виведе в консоль текст заголовку елемента (тегу <h2>) і кількість елементів в категорії (усіх <li>, вкладених в нього).
+
+const arrCategories = categoriesEl.children;
+
+console.log(arrCategories);
+
+for (const category of arrCategories) {
+  let categoryTitle = category.querySelector('h2');
+  let categoryElements = category.querySelectorAll('li');
+  console.log(`Category: ${categoryTitle.textContent}`);
+  console.log(`Elements: ${categoryElements.length}`);
+}
