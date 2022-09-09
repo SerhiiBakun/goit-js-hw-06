3ed47232ca1a3d74f0a238b77a4b9eb5b64c3b62
Serhii Bakun <gizzmo599@gmail.com>
2022-09-09 17:06:25 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -4,14 +4,6 @@ function getRandomHexColor() {
 
 // Напиши скрипт створення і очищення колекції елементів. Користувач вводить кількість елементів в input і натискає кнопку Створити, після чого рендериться колекція. Натисненням на кнопку Очистити, колекція елементів очищається.
 
-// <div id="controls">
-//   <input type="number" min="1" max="100" step="1" />
-//   <button type="button" data-create>Create</button>
-//   <button type="button" data-destroy>Destroy</button>
-// </div>
-
-// <div id="boxes"></div>
-
 // Створи функцію createBoxes(amount), яка приймає один параметр - число. Функція створює стільки <div>, скільки вказано в amount і додає їх у div#boxes.
 
 // Розміри найпершого <div> - 30px на 30px.
@@ -19,3 +11,40 @@ function getRandomHexColor() {
 // Всі елементи повинні мати випадковий колір фону у форматі HEX. Використовуй готову функцію getRandomHexColor для отримання кольору.
 
 // Створи функцію destroyBoxes(), яка очищає вміст div#boxes, у такий спосіб видаляючи всі створені елементи.
+
+const refs = {
+  inputAmount: document.querySelector('#controls > input'),
+  createBtn: document.querySelector('button[data-create]'),
+  destroyBtn: document.querySelector('button[data-destroy]'),
+  boxesArea: document.querySelector('#boxes'),
+};
+
+refs.inputAmount.addEventListener('input', getBoxesAmount);
+refs.createBtn.addEventListener('click', onCreateBoxesBtn);
+refs.destroyBtn.addEventListener('click', onDestroyBoxes);
+
+function getBoxesAmount(event) {
+  event.currentTarget.value;
+}
+
+function onCreateBoxes() {
+  let amountBoxes = refs.inputAmount.value;
+  let boxesArr = [];
+  for (let i = 0; i < amountBoxes; i += 1) {
+    const width = 30 + i * 10;
+    const height = 30 + i * 10;
+    const color = getRandomHexColor();
+    let box = `<div style='width: ${width}px; height: ${height}px;background-color:${color}'>
+    </div>`;
+    boxesArr.push(box);
+  }
+  return boxesArr.join('');
+}
+
+function onCreateBoxesBtn() {
+  refs.boxesArea.innerHTML = onCreateBoxes();
+}
+
+function onDestroyBoxes() {
+  refs.boxesArea.innerHTML = '';
+}
